140716e4c4a81a49a2595d892ee9ef8a1a27e410
minemod <yolo1234100@hotmail.com>
2024-04-05 00:52:34 -0300

    added create task card function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,38 @@ var myModal = new bootstrap.Modal(document.getElementById('taskModal'));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    let uniqueId = nextId;
+    nextId++;
+    return uniqueId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const card = document.createElement('div');
+    card.classList.add('card', 'mb-3');
 
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = task.name;
+
+    const description = document.createElement('p');
+    description.classList.add('card-text');
+    description.textContent = task.description;
+
+    const dueDate = document.createElement('p');
+    dueDate.classList.add('card-text');
+    dueDate.innerHTML = `<strong>Due Date:</strong> ${task.dueDate}`;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(description);
+    cardBody.appendChild(dueDate);
+
+    card.appendChild(cardBody);
+
+    return card;
 }
 
 // Todo: create a function to render the task list and make cards draggable
